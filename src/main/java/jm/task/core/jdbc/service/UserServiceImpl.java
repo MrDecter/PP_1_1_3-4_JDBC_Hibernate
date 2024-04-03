14d85a92c28166af3c4a8113d816751051d16256
MrDecter <ghoillus@gmail.com>
2024-04-03 15:04:49 +0300

    Внесение правок: 1. Удаление излишних коментариев 2. Изменение имени переменной

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.SQLException;
@@ -10,28 +9,28 @@ import java.util.List;
 
 // Обновление общекта на использование Hibernate
 public class UserServiceImpl implements UserService {
-    private UserDao userFunc = new UserDaoHibernateImpl();
+    private UserDao userDao = new UserDaoHibernateImpl();
     public void createUsersTable()  {
-        userFunc.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        userFunc.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) throws SQLException {
-        userFunc.saveUser(name,lastName,age);
+        userDao.saveUser(name,lastName,age);
     }
 
     public void removeUserById(long id) {
-        userFunc.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return userFunc.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        userFunc.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
